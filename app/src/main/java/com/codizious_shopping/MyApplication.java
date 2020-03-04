40e23af1160cb4dc8b2dda2d7a59409a7702b793
jai <jhirani114@rku.ac.in>
2020-03-04 11:32:06 +0530

    update by

--- app/src/main/java/com/codizious_shopping/MyApplication.java
+++ app/src/main/java/com/codizious_shopping/MyApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 import com.karumi.dexter.Dexter;
 
-//aan
+//aa
 public class MyApplication extends Application {
     @Override public void onCreate() {
         super.onCreate();
