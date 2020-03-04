eed3def958a7ce1cbb9da43abe11405f14a2c94a
Parth123-Pro <55745745+Parth123-Pro@users.noreply.github.com>
2020-03-04 10:20:35 +0530

    Updated By PARTH

--- app/src/main/java/com/codizious_shopping/MyApplication.java
+++ app/src/main/java/com/codizious_shopping/MyApplication.java
@@ -4,9 +4,7 @@ import android.app.Application;
 
 import com.karumi.dexter.Dexter;
 
-/**
- * Created by subhashsanghani on 9/12/16.
- */
+
 public class MyApplication extends Application {
     @Override public void onCreate() {
         super.onCreate();
